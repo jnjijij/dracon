de428e5f3eacdc032f63798a31a4b6de773b3534
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-09-14 20:20:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Function to fetch user data from the API and populate user blocks in index.html
+function fetchAndPopulateUsers() {
+    // Use fetch or Axios to get user data from https://jsonplaceholder.typicode.com/users
+    // Loop through the data and create user blocks in the .user-list element in index.html
+    // Add buttons to each user block and set event listeners to navigate to user-details.html
+}
+
+// Function to fetch user details and posts for a specific user in user-details.html
+function fetchUserDetails(userId) {
+    // Use fetch or Axios to get user details from https://jsonplaceholder.typicode.com/users/USER_ID
+    // Use fetch or Axios to get user's posts from https://jsonplaceholder.typicode.com/users/USER_ID/posts
+    // Populate user details and create post blocks with buttons in user-details.html
+    // Add event listeners to navigate to post-details.html for each post
+}
+
+// Function to fetch post details and comments for a specific post in post-details.html
+function fetchPostDetails(postId) {
+    // Use fetch or Axios to get post details from https://jsonplaceholder.typicode.com/posts/POST_ID
+    // Use fetch or Axios to get post comments from https://jsonplaceholder.typicode.com/posts/POST_ID/comments
+    // Populate post details and create comment blocks in post-details.html
+}
+
+// Add event listeners for navigation and data fetching as needed
